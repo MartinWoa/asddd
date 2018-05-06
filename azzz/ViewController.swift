19a54699b431d607f87aa12988027a2741062564
Wo Martin <MartinWo@martinwodemacbookpro.local>
2018-05-06 17:20:15 +0800

    Initial Commit

---
+++ azzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  azzz
+//
+//  Created by MartinWo on 2018/5/6.
+//  Copyright © 2018年 MartinWo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
